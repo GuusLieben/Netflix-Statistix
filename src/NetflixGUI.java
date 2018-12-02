7a8c66fbfa06f471db8a24e8898a86641a2a5fe4
Guus Lieben <guus@xendox.com>
2018-12-02 13:36:04 +0100

    Formatting changes
    > Add comments to explain functionality
    > Add serieOverview() to replace pink box
    > Make basic data private values
    > Minor styling changes

--- src/NetflixGUI.java
+++ src/NetflixGUI.java
@@ -1,31 +1,47 @@
 package com.netflix;
 
+import com.raphaellevy.fullscreen.*;
+
 import javax.swing.*;
 import javax.swing.border.*;
 import java.awt.*;
 
+import static com.netflix.Commons.*;
 import static java.awt.BorderLayout.*;
 import static javax.swing.JFrame.*;
 
 public class NetflixGUI {
 
   private JFrame frame;
+  private String title = "House of Cards";
+  private int episodeCount = 73;
+  private String[] choices = {
+    "House of Cards",
+    "Daredevil",
+    "Stranger Things",
+    "Orange Is the New Black",
+    "Narcos",
+    "The Crown"
+  };
 
   public NetflixGUI(int width, int height) {
     frame = new JFrame();
     setFrame(width, height);
   }
 
-  private static void addComponentsToPane(Container pane) {
+  private static void fillMenu(Container pane) {
+    // Make all content (buttons) align vertically
     pane.setLayout(new BoxLayout(pane, BoxLayout.Y_AXIS));
 
-    addAButton("Overzicht #1", pane);
-    addAButton("Overzicht #2", pane);
-    addAButton("Overzicht #3", pane);
-    addAButton("Overzicht #4", pane);
+    // Sample data
+    addButton("Overzicht #1", pane);
+    addButton("Overzicht #2", pane);
+    addButton("Overzicht #3", pane);
+    addButton("Overzicht #4", pane);
   }
 
-  private static void addAButton(String text, Container container) {
+  private static void addButton(String text, Container container) {
+    // Add button with text, align left
     JButton button = new JButton(text);
     button.setAlignmentX(Component.LEFT_ALIGNMENT);
     container.add(button);
@@ -44,19 +60,29 @@ public class NetflixGUI {
     frame.setMinimumSize(new Dimension(600, 400));
     frame.setSize(width, height);
 
+    // Add all panes
     frame.add(bottomPane(), SOUTH);
     frame.add(menu(), WEST);
     frame.add(mainPane(), CENTER);
+
+    // Make sure the application can be used full-screen on MacOS devices
+    try {
+      if (System.getProperty("os.name").startsWith("Mac"))
+        FullScreenMacOS.setFullScreenEnabled(frame, true);
+    } catch (FullScreenException ex) {
+      exception(ex);
+    }
 
     // Make the frame visible
     frame.setVisible(true);
   }
 
   private JPanel mainPane() {
+    // Create panel with 10px padding
     JPanel mainPanel = new JPanel(new BorderLayout());
-
     mainPanel.setBorder(new EmptyBorder(10, 10, 10, 10));
 
+    // Add sub-panels
     mainPanel.add(selectSeries(), NORTH);
     mainPanel.add(seriesOverview(), CENTER);
 
@@ -66,36 +92,67 @@ public class NetflixGUI {
   private JPanel seriesOverview() {
     JPanel seriesOverview = new JPanel(new BorderLayout());
 
+    // Create, center, and add padding to label
     JLabel averageViews = new JLabel("Gemiddeld 48.2% bekeken per aflevering");
     averageViews.setBorder(new EmptyBorder(5, 0, 10, 0));
     averageViews.setHorizontalAlignment(JLabel.CENTER);
-    seriesOverview.add(averageViews, NORTH);
 
-    JPanel main = new JPanel();
-    main.setBackground(Color.PINK);
-    main.setBorder(BorderFactory.createLineBorder(Color.RED));
-    seriesOverview.add(main);
+    // Add sub-panels
+    seriesOverview.add(averageViews, NORTH);
+    seriesOverview.add(serieOverview());
 
     return seriesOverview;
   }
 
+  private JPanel serieOverview() {
+    // Create panel with 1px black border
+    JPanel main = new JPanel(new BorderLayout());
+    main.setBorder(BorderFactory.createLineBorder(Color.black));
+
+    // Create inner panel with 10px padding
+    JPanel inner = new JPanel(new BorderLayout());
+    inner.setBorder(new EmptyBorder(10, 10, 10, 10));
+
+    // Sample title + formatting (using private values to ease future editing)
+    JLabel title = new JLabel(this.title);
+    title.setFont(new Font(title.getFont().getFontName(), Font.BOLD, 18));
+    // Sample count
+    JLabel episodeCount = new JLabel("No. of episodes : " + this.episodeCount);
+
+    // Create sub-panel to make locations easier
+    JPanel aboutSerieInner = new JPanel(new BorderLayout());
+
+    // New label, use <html> to allow easy formatting and wrapping of text
+    JLabel aboutSerie = new JLabel();
+    aboutSerie.setText(
+        "<html><i>A Congressman works with his equally conniving wife to exact revenge on the people who betrayed him.</i>"
+            + "<br><br><b>Creator</b>: Beau Willimon"
+            + "<br><br><b>Stars</b>: Kevin Spacey, Michel Gill, Robin WrightNewline</html>");
+    aboutSerie.setFont(
+        new Font(aboutSerie.getFont().getFontName(), Font.PLAIN, aboutSerie.getFont().getSize()));
+
+    // Add all the things!
+    aboutSerieInner.add(aboutSerie, NORTH);
+    JPanel quickView = new JPanel(new BorderLayout());
+    quickView.add(title, NORTH);
+    quickView.add(episodeCount, SOUTH);
+    inner.add(quickView, NORTH);
+    inner.add(aboutSerieInner, CENTER);
+    main.add(inner);
+
+    return main;
+  }
+
   private JPanel selectSeries() {
+    // Create dropdown with sample values
     JPanel selectSeries = new JPanel();
-    String[] choices = {
-      "House of Cards",
-      "Daredevil",
-      "Stranger Things",
-      "Orange Is the New Black",
-      "Narcos",
-      "The Crown"
-    };
-
     JLabel selectSerie = new JLabel("Selecteer een serie : ");
-    JComboBox<String> cb = new JComboBox<>(choices);
-    cb.setVisible(true);
+    JComboBox<String> comboBox = new JComboBox<>(this.choices);
+    comboBox.setVisible(true);
 
+    // Add label + dropdown
     selectSeries.add(selectSerie);
-    selectSeries.add(cb);
+    selectSeries.add(comboBox);
 
     return selectSeries;
   }
@@ -105,7 +162,7 @@ public class NetflixGUI {
     JPanel menu = new JPanel();
     menu.setBorder(new EmptyBorder(10, 0, 0, 0));
     menu.setBackground(Color.GRAY);
-    addComponentsToPane(menu);
+    fillMenu(menu);
 
     return menu;
   }
@@ -118,8 +175,10 @@ public class NetflixGUI {
     bottomPanel.setBorder(new EmptyBorder(2, 5, 2, 5));
 
     // Add labels to panel
-      JLabel creators = new JLabel("Informatica 1.2 - 23IVT1D - Guus Lieben, Tim van Wouwe, Coen Rijsdijk");
-      creators.setFont(new Font(creators.getFont().getFontName(), Font.ITALIC, creators.getFont().getSize() - 2));
+    JLabel creators =
+        new JLabel("Informatica 1.2 - 23IVT1D - Guus Lieben, Tim van Wouwe, Coen Rijsdijk");
+    creators.setFont(
+        new Font(creators.getFont().getFontName(), Font.ITALIC, creators.getFont().getSize() - 2));
     bottomPanel.add(new JLabel("Netflix Statistix"), WEST);
     bottomPanel.add(creators, EAST);
 
